ecd5fa683777e8d2282d06469a0acfffe52c4650
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-01-13 01:54:39 +1300

    Merge branch 'trial'

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 $(document).ready(function(){
-	$(".getter").submit("click", function(event) {
+	$(".query").submit("click", function(event) {
   		event.preventDefault();
-  		var searchTerm = $("input[type='text']").val();
-  		getRequest(searchTerm);
+  		var tag = $("input[type='text']").val();
+  		searchTag(tag);
   	});
+  	searchTag("potato");
+
 });
- 
-var getRequest = function(searchTerm) {
+
+var searchTag = function(tag) {
 	
-	// CS50 Food API parameters
 	var parameters = {
-		key: "eac1a9984101edd49804734c622fa622",
-	    recipe: searchTerm,
-	    output: "jsonp"
+		format: 'json',
+		max: 5,
+		q: tag,
+		api_key: "eCxqUNPKtEqBPIQtWQAdv0wBhbDjHnIr2cAd38tF"
 	};
+	
 	var result = $.ajax({
-		url: "http://api.cs50.net/food/3/facts",
+		url: "http://api.data.gov/usda/ndb/search/",
 		data: parameters,
-		dataType: "jsonp",
-		type: "GET"
+		type: "GET",
 		})
 	.done(function(result){
-		console.log(result);
+		$(".results").append("<ul class='food-list'></ul>");
+		$.each(result.list.item, function(index, value){
+			$(".food-list").append("<li><a class='foodItem' data-id='" + value.ndbno + "' href='#'>" + value.name + "</a></li>");
+		});
+		$(".foodItem").on("click", function(){
+			console.log($(this).attr("data-id"));
+
+		});
 	});
+
 };
