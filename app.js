64a009862860f331bbe477b4a553a274886bf1fd
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-01-13 20:08:55 +1300

    fixed multiple issues
    search provides results based on relevance with the ability to search
    multiple tags separated by commas

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
 	$(".query").submit("click", function(event) {
   		event.preventDefault();
-  		var userFoodQuery = $("input[type='text']").val();
-		var tag = userQuery.charAt(0).toUpperCase() + userQuery.slice(1);
-  		searchTag(tag);
-  	});
-//	searchTag("Potato");
+  		$(".results").children().remove();
 
+  		// Global variables
+  		userQuery = ($("input[type='text']").val()+",").replace(/, /g, ",");
+		multiTagsArray = $.each(userQuery.split(",").slice(0,-1), function(index, item){});
+		singleTag = multiTagsArray[0];
+
+		searchTag(singleTag);
+  	});
+//	searchTag("potato");
 });
 
-var searchTag = function(tag) {
+// Global Variables
+var userQuery,
+	multiTagsArray,
+	singleTag,
+	tagLower,
+	tagUpper,
+	checkRelavance;
+
+function searchTag(singleTag) {
 	
 	var parameters = {
 		format: 'json',
-		max: 50,
-		q: tag,
+		q: singleTag,
 		api_key: "eCxqUNPKtEqBPIQtWQAdv0wBhbDjHnIr2cAd38tF"
 	};
 	
@@ -24,17 +35,47 @@ var searchTag = function(tag) {
 		type: "GET",
 		})
 	.done(function(result){
+		var searchResults = result.list.item;
 		$(".results").append("<ul class='food-list'></ul>");
-		$.each(result.list.item, function(index, value){
-			if(value.name.indexOf(tag) == 0) {
-				$(".food-list").append("<li><a class='foodItem' data-id='" + value.ndbno + "' href='#'>" + value.name + "</a></li>");
+		if (multiTagsArray.length === 1) {
+			$.each(searchResults, function(index, searchValue) {
+				checkCaseInArray(singleTag, searchValue);
+				if(tagLower == 0 || tagUpper == 0) {
+					$(".food-list").append("<li><a class='foodItem' data-id='" + searchValue.ndbno + "' href='#'>" + searchValue.name + "</a></li>");
+				};
+			});
+		} else {
+			$.each(searchResults, function(index, searchValue) {
+				checkRelavance = 0;
+
+				var resultNamesArray =
+					$.each((searchValue.name +",")
+					.replace(/, /g, ",")
+					.replace(/ /g, ",")
+					.split(",")
+					.slice(0,-1), function(index, item){});
+
+				$.each(multiTagsArray, function(index, tagValue) {
+					checkCaseInArray(tagValue, searchValue);
+					if(tagLower >= 0 || tagUpper >= 0) {
+					checkRelavance ++;
+					};
+				});
+				if (checkRelavance===multiTagsArray.length) {
+					$(".food-list").append("<li><a class='foodItem' data-id='" + searchValue.ndbno + "' href='#'>" + searchValue.name + "</a></li>");
+				};
+			});
+		};
+		
+			function checkCaseInArray (tag, value) {
+			tagLower = value.name.indexOf(tag.charAt(0).toLowerCase() + tag.slice(1));
+			tagUpper = value.name.indexOf(tag.charAt(0).toUpperCase() + tag.slice(1));
 			};
-			console.log(value)
-		});
+
 		$(".foodItem").on("click", function(){
 			console.log($(this).attr("data-id"));
-
 		});
+
 	});
 
 };
